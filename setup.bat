fadbfadbb53ef52aa943d5a024f9e972c4989330
Elias Vitali <104759533+vitalielias@users.noreply.github.com>
2023-12-15 09:39:45 +0100

    batch file fix

--- setup.bat
+++ setup.bat
@@ -2,7 +2,6 @@ REM This batch files eliminates the need for the user to run the docker-compose 
 REM In essence, just opening this directory in the terminal (Powershell) and running docker-compose -f docker-compose.yml up -d will do the same thing.
 REM This may be useful if there are security features preventing you from running unknown .bat files (as there should be).
 @echo off
-SET "composefile=%~dp0docker-compose.yml"
 
 echo Running Docker Compose services... this may take several minutes, especially the first time.
-docker-compose -f "%composefile%" up -d
+docker-compose up
